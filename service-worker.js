16fa2e54d9ec53597fcf09f839e27f35ee0df2b3
nguyenxuandat20091985-rgb <nguyenxuandat20091985@gmail.com>
2026-09-05 05:16:10 +0700

    Improve PWA offline caching strategy

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'den-to-nghe-taxi-v4';
+const CACHE_NAME = 'den-to-nghe-taxi-v5';
 const APP_SHELL = [
   './',
   './index.html',
@@ -29,13 +29,21 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
+
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) return;
+
   event.respondWith(
     fetch(event.request)
       .then((response) => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        if (response && response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(() => {});
+        }
         return response;
       })
-      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
+      .catch(() => caches.match(event.request).then((cached) => {
+        return cached || caches.match('./index.html');
+      }))
   );
 });
