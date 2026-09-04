5be0faffff61b2979e222827fea8772c8bcbf32a
nguyenxuandat20091985-rgb <nguyenxuandat20091985@gmail.com>
2026-09-04 21:48:24 +0700

    Create js/firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBrcJEjdLxeZuyiHXwuKFVImv19HzsvxDk",
+  authDomain: "to-nghe-taxi.firebaseapp.com",
+  projectId: "to-nghe-taxi",
+  storageBucket: "to-nghe-taxi.firebasestorage.app",
+  messagingSenderId: "399059186075",
+  appId: "1:399059186075:web:d1735560897a36c789f13e",
+  measurementId: "G-VC2H66BMQ3"
+};
+
+export default firebaseConfig;
